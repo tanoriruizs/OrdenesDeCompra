1e126c58e4fc86283ab43edd3be93a650b237507
tanoriruizs <tanoriruizs@gmail.com>
2025-03-17 13:49:46 -0700

    Agregando SweetAlert para ciertas acciones

---
+++ OrdenesDeCompra/wwwroot/js/Editar.js
@@ -0,0 +1,27 @@
+﻿document.getElementById("editForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let numeroDeOrden = document.getElementById("NumeroDeOrden").value.trim();
+    let fecha = document.getElementById("Fecha").value;
+    let proveedor = document.getElementById("Proveedor").value.trim();
+    let montoTotal = document.getElementById("MontoTotal").value.trim();
+
+    if (numeroDeOrden === "" || fecha === "" || proveedor === "" || montoTotal === "") {
+        Swal.fire({
+            title: "Error",
+            text: "Todos los campos son obligatorios.",
+            icon: "error"
+        });
+        return;
+    }
+
+    Swal.fire({
+        title: "Orden Modificada",
+        text: "La orden se ha modificado correctamente.",
+        icon: "success"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            this.submit();
+        }
+    });
+});
--- OrdenesDeCompra/wwwroot/js/Index.js
+++ OrdenesDeCompra/wwwroot/js/Index.js
@@ -29,7 +29,27 @@
         },
         columnDefs: [
             { className: 'text-center', targets: '_all' },
-            { orderable: false, targets: 5 } 
+            { orderable: false, targets: 5 }
         ]
+    });
+
+    $(document).on('click', '.btn-delete', function (e) {
+        e.preventDefault();
+        var url = $(this).attr('href');
+
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: "No podrás revertir esto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Sí, eliminar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = url;
+            }
+        });
     });
 });
